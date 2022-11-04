958ca387f4b069d792778f3d9699322a0383162a
= <=>
2022-11-04 23:34:44 +0530

    implemented post-order and in-order traversal algorithms for BST

--- coding-ninjas/milestones/four/BST/BinaryTree.cpp
+++ coding-ninjas/milestones/four/BST/BinaryTree.cpp
@@ -2,49 +2,42 @@
 #include <queue>
 
 template <typename T>
-class BinaryTreeNode
-{
+class BinaryTreeNode {
 public:
     T data;
-    BinaryTreeNode<T> *left;
-    BinaryTreeNode<T> *right;
+    BinaryTreeNode<T>* left;
+    BinaryTreeNode<T>* right;
 
-    BinaryTreeNode(T data)
-    {
+    BinaryTreeNode(T data) {
         this->data = data;
-        left = NULL;
-        right = NULL;
+        this->left = nullptr;
+        this->right = nullptr;
     }
 };
 
-BinaryTreeNode<int> *takeInput()
-{
+BinaryTreeNode<int>* takeInput() {
     int rootData;
     std::cin >> rootData;
-    if (rootData == -1)
-    {
+    if (rootData == -1) {
         return NULL;
     }
-    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
-    std::queue<BinaryTreeNode<int> *> q;
+    BinaryTreeNode<int>* root = new BinaryTreeNode<int>(rootData);
+    std::queue<BinaryTreeNode<int>*> q;
     q.push(root);
-    while (!q.empty())
-    {
-        BinaryTreeNode<int> *currentNode = q.front();
+    while (!q.empty()) {
+        BinaryTreeNode<int>* currentNode = q.front();
         q.pop();
         int leftChild, rightChild;
         std::cin >> leftChild;
-        if (leftChild != -1)
-        {
-            BinaryTreeNode<int> *leftNode = new BinaryTreeNode<int>(leftChild);
+        if (leftChild != -1) {
+            BinaryTreeNode<int>* leftNode = new BinaryTreeNode<int>(leftChild);
             currentNode->left = leftNode;
             q.push(leftNode);
         }
 
         std::cin >> rightChild;
-        if (rightChild != -1)
-        {
-            BinaryTreeNode<int> *rightNode =
+        if (rightChild != -1) {
+            BinaryTreeNode<int>* rightNode =
                 new BinaryTreeNode<int>(rightChild);
             currentNode->right = rightNode;
             q.push(rightNode);
@@ -53,10 +46,8 @@ BinaryTreeNode<int> *takeInput()
     return root;
 }
 
-void preOrder(BinaryTreeNode<int> *root)
-{
-    if (root == NULL)
-    {
+void preOrder(BinaryTreeNode<int>* root) {
+    if (root == NULL) {
         return;
     }
 
@@ -64,4 +55,24 @@ void preOrder(BinaryTreeNode<int> *root)
 
     preOrder(root->left);
     preOrder(root->right);
+}
+
+void postOrder(BinaryTreeNode<int>* root) {
+    if (root == NULL) {
+        return;
+    }
+
+    postOrder(root->left);
+    postOrder(root->right);
+    std::cout << root->data << " ";
+}
+
+void inOrder(BinaryTreeNode<int>* root) {
+    if (root == NULL) {
+        return;
+    }
+
+    inOrder(root->left);
+    std::cout << root->data << " ";
+    inOrder(root->right);
 }
