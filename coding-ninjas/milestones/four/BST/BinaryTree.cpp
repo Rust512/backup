392ad1115781b46e3cec72d0c5647583ff1bd4e7
= <=>
2022-11-04 23:31:50 +0530

    implemented BST with input and output functions

---
+++ coding-ninjas/milestones/four/BST/BinaryTree.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <queue>
+
+template <typename T>
+class BinaryTreeNode
+{
+public:
+    T data;
+    BinaryTreeNode<T> *left;
+    BinaryTreeNode<T> *right;
+
+    BinaryTreeNode(T data)
+    {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+BinaryTreeNode<int> *takeInput()
+{
+    int rootData;
+    std::cin >> rootData;
+    if (rootData == -1)
+    {
+        return NULL;
+    }
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+    std::queue<BinaryTreeNode<int> *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        BinaryTreeNode<int> *currentNode = q.front();
+        q.pop();
+        int leftChild, rightChild;
+        std::cin >> leftChild;
+        if (leftChild != -1)
+        {
+            BinaryTreeNode<int> *leftNode = new BinaryTreeNode<int>(leftChild);
+            currentNode->left = leftNode;
+            q.push(leftNode);
+        }
+
+        std::cin >> rightChild;
+        if (rightChild != -1)
+        {
+            BinaryTreeNode<int> *rightNode =
+                new BinaryTreeNode<int>(rightChild);
+            currentNode->right = rightNode;
+            q.push(rightNode);
+        }
+    }
+    return root;
+}
+
+void preOrder(BinaryTreeNode<int> *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    std::cout << root->data << " ";
+
+    preOrder(root->left);
+    preOrder(root->right);
+}
