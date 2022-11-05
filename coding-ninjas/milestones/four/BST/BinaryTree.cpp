6d56e85c1bc1bc3a5ad25728fa59c55f5eb4eed2
= <=>
2022-11-05 20:12:41 +0530

    added new method to print the each level of the binary tree on a new line

--- coding-ninjas/milestones/four/BST/BinaryTree.cpp
+++ coding-ninjas/milestones/four/BST/BinaryTree.cpp
@@ -81,3 +81,28 @@ void inOrder(BinaryTreeNode<int>* root) {
     std::cout << root->data << " ";
     inOrder(root->right);
 }
+
+void printLevelATNewLine(BinaryTreeNode<int>* root) {
+    queue<BinaryTreeNode<int>*> q;
+    q.push(root);
+    q.push(NULL);
+    while (!q.empty()) {
+        BinaryTreeNode<int>* first = q.front();
+        q.pop();
+        if (first == NULL) {
+            if (q.empty()) {
+                break;
+            }
+            cout << endl;
+            q.push(NULL);
+            continue;
+        }
+        cout << first->data << " ";
+        if (first->left != NULL) {
+            q.push(first->left);
+        }
+        if (first->right != NULL) {
+            q.push(first->right);
+        }
+    }
+}
