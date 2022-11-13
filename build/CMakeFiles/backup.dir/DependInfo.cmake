438c176ca7119d56504a2ba2709026b3444d1555
= <=>
2022-11-13 14:34:10 +0530

    CMake config for visual studio code

---
+++ build/CMakeFiles/backup.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/devang/code/backup/code-chef/starters/64/broken-string/solution.cpp" "CMakeFiles/backup.dir/code-chef/starters/64/broken-string/solution.cpp.o" "gcc" "CMakeFiles/backup.dir/code-chef/starters/64/broken-string/solution.cpp.o.d"
+  "/home/devang/code/backup/code-chef/starters/64/distinct-colors/solution.cpp" "CMakeFiles/backup.dir/code-chef/starters/64/distinct-colors/solution.cpp.o" "gcc" "CMakeFiles/backup.dir/code-chef/starters/64/distinct-colors/solution.cpp.o.d"
+  "/home/devang/code/backup/code-chef/starters/64/luigi-and-uniformity/solution.cpp" "CMakeFiles/backup.dir/code-chef/starters/64/luigi-and-uniformity/solution.cpp.o" "gcc" "CMakeFiles/backup.dir/code-chef/starters/64/luigi-and-uniformity/solution.cpp.o.d"
+  "/home/devang/code/backup/code-chef/starters/64/range-assign/solution.cpp" "CMakeFiles/backup.dir/code-chef/starters/64/range-assign/solution.cpp.o" "gcc" "CMakeFiles/backup.dir/code-chef/starters/64/range-assign/solution.cpp.o.d"
+  "/home/devang/code/backup/coding-ninjas/milestones/four/BST/BST-from-sorted-array/solution.cpp" "CMakeFiles/backup.dir/coding-ninjas/milestones/four/BST/BST-from-sorted-array/solution.cpp.o" "gcc" "CMakeFiles/backup.dir/coding-ninjas/milestones/four/BST/BST-from-sorted-array/solution.cpp.o.d"
+  "/home/devang/code/backup/coding-ninjas/milestones/four/BST/BST-to-sorted-linked-list/solution.cpp" "CMakeFiles/backup.dir/coding-ninjas/milestones/four/BST/BST-to-sorted-linked-list/solution.cpp.o" "gcc" "CMakeFiles/backup.dir/coding-ninjas/milestones/four/BST/BST-to-sorted-linked-list/solution.cpp.o.d"
+  "/home/devang/code/backup/coding-ninjas/milestones/four/BST/BinaryTree.cpp" "CMakeFiles/backup.dir/coding-ninjas/milestones/four/BST/BinaryTree.cpp.o" "gcc" "CMakeFiles/backup.dir/coding-ninjas/milestones/four/BST/BinaryTree.cpp.o.d"
+  "/home/devang/code/backup/coding-ninjas/milestones/four/BST/LinkedList.cpp" "CMakeFiles/backup.dir/coding-ninjas/milestones/four/BST/LinkedList.cpp.o" "gcc" "CMakeFiles/backup.dir/coding-ninjas/milestones/four/BST/LinkedList.cpp.o.d"
+  "/home/devang/code/backup/coding-ninjas/milestones/four/BST/exercises/LCA-of-a-BST/solution.cpp" "CMakeFiles/backup.dir/coding-ninjas/milestones/four/BST/exercises/LCA-of-a-BST/solution.cpp.o" "gcc" "CMakeFiles/backup.dir/coding-ninjas/milestones/four/BST/exercises/LCA-of-a-BST/solution.cpp.o.d"
+  "/home/devang/code/backup/coding-ninjas/milestones/four/BST/exercises/LCA-of-a-binary-tree/solution.cpp" "CMakeFiles/backup.dir/coding-ninjas/milestones/four/BST/exercises/LCA-of-a-binary-tree/solution.cpp.o" "gcc" "CMakeFiles/backup.dir/coding-ninjas/milestones/four/BST/exercises/LCA-of-a-binary-tree/solution.cpp.o.d"
+  "/home/devang/code/backup/coding-ninjas/milestones/four/BST/exercises/create-and-insert-duplicate-node/solution.cpp" "CMakeFiles/backup.dir/coding-ninjas/milestones/four/BST/exercises/create-and-insert-duplicate-node/solution.cpp.o" "gcc" "CMakeFiles/backup.dir/coding-ninjas/milestones/four/BST/exercises/create-and-insert-duplicate-node/solution.cpp.o.d"
+  "/home/devang/code/backup/coding-ninjas/milestones/four/BST/exercises/largest-BST/solution.cpp" "CMakeFiles/backup.dir/coding-ninjas/milestones/four/BST/exercises/largest-BST/solution.cpp.o" "gcc" "CMakeFiles/backup.dir/coding-ninjas/milestones/four/BST/exercises/largest-BST/solution.cpp.o.d"
+  "/home/devang/code/backup/coding-ninjas/milestones/four/BST/exercises/pair-sum-binary-tree/solution.cpp" "CMakeFiles/backup.dir/coding-ninjas/milestones/four/BST/exercises/pair-sum-binary-tree/solution.cpp.o" "gcc" "CMakeFiles/backup.dir/coding-ninjas/milestones/four/BST/exercises/pair-sum-binary-tree/solution.cpp.o.d"
+  "/home/devang/code/backup/coding-ninjas/milestones/four/BST/exercises/replace-with-sum-of-greater-nodes/solution.cpp" "CMakeFiles/backup.dir/coding-ninjas/milestones/four/BST/exercises/replace-with-sum-of-greater-nodes/solution.cpp.o" "gcc" "CMakeFiles/backup.dir/coding-ninjas/milestones/four/BST/exercises/replace-with-sum-of-greater-nodes/solution.cpp.o.d"
+  "/home/devang/code/backup/coding-ninjas/milestones/four/BST/find-path-in-BST/solution.cpp" "CMakeFiles/backup.dir/coding-ninjas/milestones/four/BST/find-path-in-BST/solution.cpp.o" "gcc" "CMakeFiles/backup.dir/coding-ninjas/milestones/four/BST/find-path-in-BST/solution.cpp.o.d"
+  "/home/devang/code/backup/coding-ninjas/milestones/four/BST/implement-BST/solution.cpp" "CMakeFiles/backup.dir/coding-ninjas/milestones/four/BST/implement-BST/solution.cpp.o" "gcc" "CMakeFiles/backup.dir/coding-ninjas/milestones/four/BST/implement-BST/solution.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
